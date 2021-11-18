c2f98e1326f36109c2187387bf0f01ec631b9ef3
Miłosz Robak <milosz@wandlee.com>
2021-11-18 19:13:38 +0100

    zadanie lab3

---
+++ lab3/Models/FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab3.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imię")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Ilość powtórzeń")]
+        [Range(1,5)]
+        public int Repetition { get; set; }
+    }
+}
